2f6c40cc0f14cf66a09f91d2b454f8ee1d7c52f8
Shresth <vermashresth@gmail.com>
2017-08-13 18:14:03 +0530

    working

--- dcGAN/dcgangit.py
+++ dcGAN/dcgangit.py
@@ -1,12 +1,22 @@
-import keras
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 13 16:25:24 2017
+
+@author: petrichor
+"""
+
 import numpy as np
-import pandas as pd
+import time
+from tensorflow.examples.tutorials.mnist import input_data
 
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Reshape, InputLayer, Conv2D, Dropout, Activation,BatchNormalization, UpSampling2D, Conv2DTranspose
-
-from keras.layers.advanced_activations import LeakyReLU 
-from keras.optimizers import RMSprop
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop
+import pandas as pd
+import matplotlib.pyplot as plt
 
 train=pd.read_csv("/home/petrichor/Downloads/mnist/train.csv")
 
@@ -18,17 +28,32 @@ print len(temp)
 train_x=np.stack(temp)
 
 
-class adv():
-   def __init__(self,img_rows=28,img_cols=28,channel=1):
+class ElapsedTimer(object):
+    def __init__(self):
+        self.start_time = time.time()
+    def elapsed(self,sec):
+        if sec < 60:
+            return str(sec) + " sec"
+        elif sec < (60 * 60):
+            return str(sec / 60) + " min"
+        else:
+            return str(sec / (60 * 60)) + " hr"
+    def elapsed_time(self):
+        print("Elapsed: %s " % self.elapsed(time.time() - self.start_time) )
+
+class DCGAN(object):
+    def __init__(self, img_rows=28, img_cols=28, channel=1):
+
         self.img_rows = img_rows
         self.img_cols = img_cols
         self.channel = channel
         self.D = None   # discriminator
         self.G = None   # generator
         self.AM = None  # adversarial model
-        self.DM = None
-        
-   def discriminator(self):
+        self.DM = None  # discriminator model
+
+    # (W−F+2P)/S+1
+    def discriminator(self):
         if self.D:
             return self.D
         self.D = Sequential()
@@ -61,8 +86,7 @@ class adv():
         self.D.summary()
         return self.D
         
-        
-   def generator(self):
+    def generator(self):
         if self.G:
             return self.G
         self.G = Sequential()
@@ -98,7 +122,8 @@ class adv():
         self.G.summary()
         return self.G
         
-   def discriminator_model(self):
+        
+    def discriminator_model(self):
         if self.DM:
             return self.DM
         optimizer = RMSprop(lr=0.0002, decay=6e-8)
@@ -108,7 +133,7 @@ class adv():
             metrics=['accuracy'])
         return self.DM
 
-   def adversarial_model(self):
+    def adversarial_model(self):
         if self.AM:
             return self.AM
         optimizer = RMSprop(lr=0.0001, decay=3e-8)
@@ -128,34 +153,36 @@ class MNIST_DCGAN(object):
 
         self.x_train=train_x
 
-        self.DCGAN = adv()
+        self.DCGAN = DCGAN()
         self.discriminator =  self.DCGAN.discriminator_model()
         self.adversarial = self.DCGAN.adversarial_model()
         self.generator = self.DCGAN.generator()
-        
-    def train(self, train_steps=2000, batch_size=256, save_interval=0): 
-            noise_input = None
+
+    def train(self, train_steps=2000, batch_size=256, save_interval=0):
+        noise_input = None
+        if save_interval>0:
+            noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
+        for i in range(train_steps):
+            images_train = self.x_train[np.random.randint(0,
+                self.x_train.shape[0], size=batch_size), :, :, :]
+            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            images_fake = self.generator.predict(noise)
+            x = np.concatenate((images_train, images_fake))
+            y = np.ones([2*batch_size, 1])
+            y[batch_size:, :] = 0
+            d_loss = self.discriminator.train_on_batch(x, y)
+
+            y = np.ones([batch_size, 1])
+            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            a_loss = self.adversarial.train_on_batch(noise, y)
+            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
+            print(log_mesg)
             if save_interval>0:
-                noise_input=np.random.uniform(-1.0,1.0,size=[16,100])
-            for i in range(train_steps):
-                images_train=self.x_train[np.random.randint(0,self.x_train.shape[0],size=batch_size),:,:,:]
-                noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-                images_fake=self.generator.predict(noise)
-                x=np.concatenate((images_train,images_fake))
-                y=np.ones([batch_size*2,1])
-                y[batch_size:,:]=0
-                d_loss=self.discriminator.train_on_batch(x,y)
-                
-                y=np.ones([batch_size,1])
-                noise=np.random.uniform(-1.0,1.0,size=[batch_size,100])
-                a_loss=self.adversarial.train_on_batch(noise,y)
-                log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
-                log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
-                print(log_mesg,i)
-                if save_interval>0:
-                    if (i+1)%save_interval==0:
-                        self.plot_images(save2file=True, samples=noise_input.shape[0],noise=noise_input, step=(i+1))
-                        print("heyHaritha")
+                if (i+1)%save_interval==0:
+                    self.plot_images(save2file=True, samples=noise_input.shape[0],\
+                        noise=noise_input, step=(i+1))
+
     def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0):
         filename = 'mnist.png'
         if fake:
@@ -181,20 +208,11 @@ class MNIST_DCGAN(object):
             plt.close('all')
         else:
             plt.show()
-            
-if __name__=='__main__':
-    
+
+if __name__ == '__main__':
     mnist_dcgan = MNIST_DCGAN()
-    #timer = ElapsedTimer()
+    timer = ElapsedTimer()
     mnist_dcgan.train(train_steps=10000, batch_size=256, save_interval=500)
-    #imer.elapsed_time()
+    timer.elapsed_time()
     mnist_dcgan.plot_images(fake=True)
     mnist_dcgan.plot_images(fake=False, save2file=True)
-                
-        
-        
-        
-        
-        
-        
-        
